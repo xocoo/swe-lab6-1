32e986ced533834033c4d65b65edeae31489bc1a
Khosbayar <xocoooo@gmail.com>
2022-06-30 21:37:44 -0500

    update secondMax

---
+++ studentrecords/src/main/java/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/App.java
@@ -0,0 +1,11 @@
+package edu.mum.cs.cs425.demos.studentrecordsmgmtapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- studentrecords/src/main/java/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/MyStudentRecordsMgmtApp.java
+++ studentrecords/src/main/java/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/MyStudentRecordsMgmtApp.java
@@ -3,6 +3,7 @@ package edu.mum.cs.cs425.demos.studentrecordsmgmtapp;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.Comparator;
 
 import edu.mum.cs.cs425.demos.studentrecordsmgmtapp.model.Student;
@@ -19,9 +20,18 @@ public class MyStudentRecordsMgmtApp {
             }
         };
 
-        // printListOfStudents(students);
+        System.out.println("All students:");
+        printListOfStudents(students);
+
+        System.out.println("Platinum students:");
         List<Student> studentsPlatinum = getListOfPlatinumAlumniStudents(students);
-        studentsPlatinum.stream().sorted().forEach(System.out::println);
+        for (Student student : studentsPlatinum) {
+            System.out.println(student);
+        }
+
+        int[] intArray = new int[] { 55, 10, 60, 14, 21, 35 };
+        printHelloWorld(intArray);
+        System.out.println("Second Max: " + findSecondBiggest(intArray));
     }
 
     public static void printListOfStudents(List<Student> students) {
@@ -30,13 +40,39 @@ public class MyStudentRecordsMgmtApp {
 
     public static List<Student> getListOfPlatinumAlumniStudents(List<Student> students) {
         List<Student> studentsPlatinum = new ArrayList<>();
+        LocalDate platinumYear = LocalDate.now().minusYears(30);
         for (Student student : students) {
-            LocalDate platinumYear = LocalDate.now().minusYears(30);
             boolean isBefore = student.getDateOfAdmission().isBefore(platinumYear);
             if (isBefore) {
                 studentsPlatinum.add(student);
             }
         }
-        return studentsPlatinum;
+        return studentsPlatinum.stream().sorted(Comparator.comparing(Student::getDateOfAdmission).reversed())
+                .collect(Collectors.toList());
+    }
+
+    public static void printHelloWorld(int[] intarray) {
+        for (int i = 0; i < intarray.length; i++) {
+            if (intarray[i] % 5 == 0 && intarray[i] % 7 == 0)
+                System.out.println("HelloWorld");
+            else if (intarray[i] % 5 == 0)
+                System.out.println("Hello");
+            else if (intarray[i] % 7 == 0)
+                System.out.println("World");
+
+        }
+    }
+
+    public static int findSecondBiggest(int[] intArray) {
+        int max = intArray[0];
+        int secondMax = intArray[0];
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] > max) {
+                secondMax = max;
+                max = intArray[i];
+            } else if (intArray[i] > secondMax)
+                secondMax = intArray[i];
+        }
+        return secondMax;
     }
 }
